28e48de7dec8980ac13a973d447fffe9f4638022
Robotechnic <robotechnic.developeur@gmail.com>
2021-04-16 18:31:28 +0200

    add french

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express")
 var app = express()
+const cookie = require("cookie-parser")
+
+app.use(cookie(process.env.SECRET || "testSecret"))
 
 const port = process.env.PORT || 8080
+
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname+"/public"))
@@ -9,9 +13,22 @@ app.use('/build/', express.static(__dirname+'/node_modules/three/build'));
 app.use('/exemples/', express.static(__dirname+'/node_modules/three/examples/jsm'));
 
 app.get("",(req,res)=>{
-	res.render("index.ejs")
+	var lang = "en"
+	if(req.cookies.lang){
+		lang = req.cookies.lang
+	}
+	res.render("index.ejs",{lang:lang})
 })
 
+app.get("/fr",(req,res)=>{
+	res.cookie("lang","fr")
+	res.render("index.ejs",{lang:"fr"})
+})
+
+app.get("/en",(req,res)=>{
+	res.cookie("lang","en")
+	res.render("index.ejs",{lang:"en"})
+})
 
 
 app.listen(port,()=>{
